5e9d074277cc7df0b090b91ed4ab31656057432c
Vladislav <zippo585@gmail.com>
2021-01-08 23:13:46 +0500

    Добавил логику окончания игры

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-var characterRef = document.getElementById("character");
-var blockRef = document.getElementById("block");
+const characterRef = document.getElementById("character");
+const blockRef = document.getElementById("block");
 
 /**
  * Прыгать персонажем
@@ -14,5 +14,22 @@ function jump() {
 }
 
 var checkDead = setInterval(() => {
-	window.getComputedStyle(characterRef).getPropertyValue("top");
+	const characterTop = parseInt(
+		window.getComputedStyle(characterRef).getPropertyValue("top")
+	);
+
+	const blockLeft = parseInt(
+		window.getComputedStyle(blockRef).getPropertyValue("left")
+	);
+
+	/*Величины прыжка 50 px вверх прыгает он вверх 
+	то есть когда он начинает опускаться его top увеличивается
+	поэтому когда он меньше 130 он соприкасается с блоком поэтому и проигрыш
+	*/
+	if (blockLeft < 20 && blockLeft > 0 && characterTop >= 130) {
+		console.dir([characterTop, blockLeft]);
+		blockRef.style.animation = "none";
+		blockRef.style.display = "none";
+		alert("You lose!");
+	}
 }, 10);
